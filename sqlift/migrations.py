30a3bf4edddf8908408e99687651eed47ceb4f6b
SpaceShaman <spaceshaman@tuta.io>
2024-12-02 19:39:02 +0100

    ♻️ (migrations.py): refactor to extract SQL command retrieval into a separate function for improved readability and maintainability

--- sqlift/migrations.py
+++ sqlift/migrations.py
@@ -1,10 +1,12 @@
 import sqlite3
 
 
-def up(filename: str) -> None:
-    sql = open(f"migrations/{filename}.sql").read()
-
-    sql = sql.split("--DOWN")[0]
+def up(migration_name: str) -> None:
     with sqlite3.connect("db.sqlite") as connection:
         cursor = connection.cursor()
-        cursor.execute(sql)
+        cursor.execute(_get_sql_up_command(migration_name))
+
+
+def _get_sql_up_command(migration_name: str) -> str:
+    sql = open(f"migrations/{migration_name}.sql").read()
+    return sql.split("--DOWN")[0]
