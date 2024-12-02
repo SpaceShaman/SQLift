318ef2ab84048c110778e06b7541a061b4a28069
SpaceShaman <spaceshaman@tuta.io>
2024-12-02 19:41:31 +0100

    ♻️ (migrations.py): refactor SQL execution into a separate function to improve code reusability and readability

--- sqlift/migrations.py
+++ sqlift/migrations.py
@@ -2,11 +2,15 @@ import sqlite3
 
 
 def up(migration_name: str) -> None:
-    with sqlite3.connect("db.sqlite") as connection:
-        cursor = connection.cursor()
-        cursor.execute(_get_sql_up_command(migration_name))
+    _execute_sql(_get_sql_up_command(migration_name))
 
 
 def _get_sql_up_command(migration_name: str) -> str:
     sql = open(f"migrations/{migration_name}.sql").read()
     return sql.split("--DOWN")[0]
+
+
+def _execute_sql(sql: str) -> None:
+    with sqlite3.connect("db.sqlite") as connection:
+        cursor = connection.cursor()
+        cursor.execute(sql)
